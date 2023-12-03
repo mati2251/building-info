77899eb4dc31a4088c8088dba34a9817fd9dd404
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-12-03 13:16:49 +0100

    Check logger

--- src/main/java/pl/put/bulidinginfo/HelloWorldController.java
+++ src/main/java/pl/put/bulidinginfo/HelloWorldController.java
@@ -2,11 +2,17 @@ package pl.put.bulidinginfo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class HelloWorldController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
     @GetMapping("/")
     public String helloWorld() {
+        logger.info("Hello World!");
         return "Hello World!";
     }
 }
