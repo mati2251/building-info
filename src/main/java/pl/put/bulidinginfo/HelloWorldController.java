0f217f359e3f323e2d14132f92a1787b927907b4
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-12-03 13:22:08 +0100

    Serialization check

--- src/main/java/pl/put/bulidinginfo/HelloWorldController.java
+++ src/main/java/pl/put/bulidinginfo/HelloWorldController.java
@@ -1,6 +1,7 @@
 package pl.put.bulidinginfo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +12,12 @@ public class HelloWorldController {
     private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
     @GetMapping("/")
-    public String helloWorld() {
+    public String helloWorld(@RequestBody HelloWorldRequestBody body){
         logger.info("Hello World!");
-        return "Hello World!";
+        return "Hello World " + body.name + "!";
+    }
+
+    static class HelloWorldRequestBody {
+        public String name;
     }
 }
