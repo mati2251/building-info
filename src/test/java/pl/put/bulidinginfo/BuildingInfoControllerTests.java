c495a874e91022d2801cf092a8a61a2ccbf125d1
McQbis <kubismaciej379@gmail.com>
2024-01-21 02:30:31 +0100

    5 mock-test methods for building info controller

--- src/test/java/pl/put/bulidinginfo/BuildingInfoControllerTests.java
+++ src/test/java/pl/put/bulidinginfo/BuildingInfoControllerTests.java
@@ -10,6 +10,9 @@ import pl.put.bulidinginfo.models.Type;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 public class BuildingInfoControllerTests {
@@ -34,6 +37,13 @@ public class BuildingInfoControllerTests {
     }
 
     @Test
+    public void testOfCallsGetArea(){
+        Room building = mock(Room.class);
+        buildingInfoController.getArea(building);
+        verify(building, times(2)).getArea();
+    }
+
+    @Test
     public void testGetCube() {
         Room room = new Room(1, "Room", 10.0f, 30.0f, 0.0f, 0);
         Room room2 = new Room(2, "Room2", 20.0f, 40.0f, 0.0f, 0);
@@ -43,6 +53,13 @@ public class BuildingInfoControllerTests {
         RoomCompound floor2 = new RoomCompound(6, "Floor2", Type.FLOOR, List.of(room3, room4));
         RoomCompound building = new RoomCompound(7, "Building", Type.BUILDING, List.of(floor, floor2));
         assertEquals(buildingInfoController.getCube(building), 140.0f);
+    }
+
+    @Test
+    public void testOfCallsGetCube(){
+        Room building = mock(Room.class);
+        buildingInfoController.getCube(building);
+        verify(building, times(2)).getCube();
     }
 
     @Test
@@ -59,6 +76,13 @@ public class BuildingInfoControllerTests {
     }
 
     @Test
+    public void testOfCallsGetLocationsWithHigherHeatingRateForBuilding(){
+        Room building = mock(Room.class);
+        buildingInfoController.getLocationsWithHigherHeatingRate(building, 1.0f);
+        verify(building, times(2)).getLocationsWithHigherHeatingRate(1.0f);
+    }
+
+    @Test
     public void getHeatingPerCube() {
         Room room = new Room(1, "Room", 10.0f, 30.0f, 25.0f, 0);
         Room room2 = new Room(2, "Room2", 20.0f, 40.0f, 30.0f, 0);
@@ -72,6 +96,13 @@ public class BuildingInfoControllerTests {
     }
 
     @Test
+    public void testOfCallsGetHeatingPerCubeMeter(){
+        Room building = mock(Room.class);
+        buildingInfoController.getHeatingPerCube(building);
+        verify(building, times(2)).getHeatingPerCubeMeter();
+    }
+
+    @Test
     public void getLightingPower() {
         Room room = new Room(1, "Room", 10.0f, 30.0f, 25.0f, 150);
         Room room2 = new Room(2, "Room2", 20.0f, 40.0f, 30.0f, 250);
@@ -82,5 +113,12 @@ public class BuildingInfoControllerTests {
         RoomCompound building = new RoomCompound(7, "Building", Type.BUILDING, List.of(floor, floor2));
 
         assertEquals(buildingInfoController.getLightingPower(building), 8.5f);
+    }
+
+    @Test
+    public void testOfCallsGetLightingPower(){
+        Room building = mock(Room.class);
+        buildingInfoController.getLightingPower(building);
+        verify(building, times(2)).getLightingPower();
     }
 }
